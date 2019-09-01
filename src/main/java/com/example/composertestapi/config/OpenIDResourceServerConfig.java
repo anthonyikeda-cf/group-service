e24a7e6b2e214f06290d28c8eae8fbd38a963443
Anthony Ikeda <anthony.ikeda@gmail.com>
2019-09-01 10:20:25 -0700

    Fixed the build, need to test to see if the permissions still work

--- src/main/java/com/example/composertestapi/config/OpenIDResourceServerConfig.java
+++ src/main/java/com/example/composertestapi/config/OpenIDResourceServerConfig.java
@@ -3,10 +3,12 @@ package com.example.composertestapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+@EnableWebSecurity
 @EnableResourceServer
 @Configuration
 public class OpenIDResourceServerConfig extends ResourceServerConfigurerAdapter {
--- src/test/java/com/example/composertestapi/GroupApplicationTests.java
+++ src/test/java/com/example/composertestapi/GroupApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.composertestapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, ReactiveManagementWebSecurityAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class GroupApplicationTests {
 
     @Test
